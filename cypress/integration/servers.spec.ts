5650739c56926f11416a173cc7567cbdf7e35d1f
piotrpekala7 <31202938+piotrpekala7@users.noreply.github.com>
2020-11-02 20:04:19 +0100

    Simplify server tests

--- cypress/integration/servers.spec.ts
+++ cypress/integration/servers.spec.ts
@@ -1,22 +1,28 @@
 context('Server', () => {
+    const url = 'http://localhost:4200/servers';
+    const addDetectedServerButtonSelector = 'body > app-root > app-default-layout > main > app-server-list > div > div.default-content > app-server-discovery > mat-card > mat-card-actions > button:nth-child(2)';
+    const addServerButtonSelector = 'body > app-root > app-default-layout > main > app-server-list > div > div.default-content > div.buttons-bar > button';
+    const addedServerSelector = 'body > app-root > app-default-layout > main > app-server-list > div > div.default-content > div.mat-elevation-z8 > mat-table > mat-row';
+
     beforeEach(() => {
-      cy.visit('http://localhost:4200/servers');
+      cy.visit(url);
     });
   
-    it('Add server button should be visible', () => {
-        cy.get('body > app-root > app-default-layout > main > app-server-list > div > div.default-content > div.buttons-bar > button');
+    specify('Add server button should be visible', () => {
+        cy.get(addServerButtonSelector);
     });
 
-    it('User should have possibility to add detected server', () => {
-        cy.get('body > app-root > app-default-layout > main > app-server-list > div > div.default-content > app-server-discovery > mat-card > mat-card-actions > button:nth-child(2)');
+    specify('User should have possibility to add detected server', () => {
+        if (!cy.get(addedServerSelector)) {
+            cy.get(addDetectedServerButtonSelector);
+        }
     });
 
-    it('User should see added server in the list', () => {
-        cy.get('body > app-root > app-default-layout > main > app-server-list > div > div.default-content > app-server-discovery > mat-card > mat-card-actions > button:nth-child(2)')
-            .click();
-
-        cy.wait(100);
-
-        cy.get('body > app-root > app-default-layout > main > app-server-list > div > div.default-content > div.mat-elevation-z8 > mat-table > mat-row');
+    specify('User should see added server in the list', () => {
+        if (!cy.get(addedServerSelector)) {
+            cy.get(addDetectedServerButtonSelector)
+                .click();
+            cy.get(addedServerSelector);   
+        }
     });
 });
